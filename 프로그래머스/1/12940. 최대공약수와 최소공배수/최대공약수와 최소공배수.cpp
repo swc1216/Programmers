babc831bb4fbe064a46c7dc36cab3e5a626243e2
LEAAON <luknightsin@gmail.com>
2026-07-20 16:21:30 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -3,20 +3,7 @@
 
 using namespace std;
 
-vector<int> solution(int n, int m)
-{
-    int originalN = n;
-    int originalM = m;
-
-    while (m != 0)
-    {
-        int remainder = n % m;
-        n = m;
-        m = remainder;
-    }
-
-    int gcd = n;
-    int lcm = originalN / gcd * originalM;
-
-    return {gcd, lcm};
+vector<int> solution(int n, int m) {
+    vector<int> answer;
+    return answer;
 }
